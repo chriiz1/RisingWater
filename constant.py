c4486d69fc8b95d53140758e911272709bfac2f9
Christoph Proß <christoph.pross@gmx.at>
2022-02-14 17:29:17 +0100

    water model v1

--- constant.py
+++ constant.py
@@ -1,13 +1,20 @@
 CANVAS_START=20
 
-WINDOW_WIDTH=800
+WINDOW_WIDTH=1000
 
-WINDOW_HEIGHT=600
+WINDOW_HEIGHT=850
 
-DEFAULT_SPEED = 6
+DEFAULT_SPEED = 7
 
 BLOCK_SIZE = 30
 
-BLOCK_COLOR = {"air": "white", "water": "blue", "earth": "brown"}
+BLOCK_COLOR = {"air": "white", "water": "blue", "earth": "brown", "water_entry": "black", "water_exit": "gray"}
 
 SPEED_STEPS = 11
+
+EXTEND_X = 30
+
+EXTEND_Y = 25
+
+
+BTN_START_STOP_TEXT = {False: 'Run', True: 'Stop'}
--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def get_rectangle_ref(world, x_pos, y_pos):
         return None, None
     index_x = int((x_pos - constant.CANVAS_START) / constant.BLOCK_SIZE)
     index_y = int((y_pos - constant.CANVAS_START) / constant.BLOCK_SIZE)
-    block_ref = f'{index_x}/{index_y}'
+    block_ref = (index_x, index_y)
     pol_ref = world.blocks[block_ref].pol_ref
     return pol_ref, block_ref
 
@@ -22,7 +22,19 @@ def change_block(event, world):
     if pol_ref is not None:
         color = constant.BLOCK_COLOR[world.selected_block_type]
         world.canvas.itemconfig(pol_ref, fill = color)
-        world.blocks[block_ref].type = world.selected_block_type
+
+        if block_ref in world.water_exits:
+            world.water_exits.remove(block_ref)
+        if block_ref in world.water_entries:
+            world.water_entries.remove(block_ref)
+        if world.selected_block_type == 'water_entry' and block_ref not in world.water_entries:
+            world.water_entries.append(block_ref)
+            world.blocks[block_ref].type = 'water'
+        elif world.selected_block_type == 'water_exit'and block_ref not in world.water_exits:
+            world.water_exits.append(block_ref)
+            world.blocks[block_ref].type = 'air'
+        else:
+            world.blocks[block_ref].type = world.selected_block_type
 
 def on_down(event, world):
     world.drawing = True
@@ -58,21 +70,27 @@ def setup_ui(world):
     ui = UI(root, canvas, speed_widget)
 
     # start/stop
-    btn_start = Button(root, text ="Start", command = ui.start)
-    btn_start.pack(side = LEFT, padx=20)
-    btn_stop = Button(root, text ="Stop", command = ui.stop)
-    btn_stop.pack(side = LEFT)
-    speed_widget.pack(side = LEFT, padx=50)
-    btn_save = Button(root, text ="Save", command = world.save)
+    btn_start_stop = Button(root, textvariable=ui.start_stop_text, command = ui.toogle_start_stop, width=10)
+    btn_start_stop.pack(side = LEFT, padx=20)
+    btn_clear_all = Button(root, text= "Clear world", command = ui.clear_world, width=9,activebackground='#ff4444')
+    btn_clear_all.pack(side = LEFT, padx=20)
+    speed_widget.pack(side = LEFT, padx=(20,50))
+    btn_save = Button(root, text ="Save default", command = world.save)
     btn_save.pack(side = LEFT)
-    btn_load = Button(root, text ="Load", command = world.load)
-    btn_load.pack(side = LEFT, padx=20)
+    btn_load = Button(root, text ="Load default", command = world.load)
+    btn_load.pack(side = LEFT, padx=(10,30))
+    btn_save = Button(root, text ="Save as", command = world.save2)
+    btn_save.pack(side = LEFT)
+    btn_load = Button(root, text ="Load ", command = world.load2)
+    btn_load.pack(side = LEFT)
 
     # context
     m = Menu(root, tearoff = 0)
     m.add_command(label ="air", command = lambda arg1 = "air": world.set_selected_block_type(arg1))
     m.add_command(label ="water", command = lambda arg1 = "water": world.set_selected_block_type(arg1))
     m.add_command(label ="earth", command = lambda arg1 = "earth": world.set_selected_block_type(arg1))
+    m.add_command(label ="water_entry", command = lambda arg1 = "water_entry": world.set_selected_block_type(arg1))
+    m.add_command(label ="water_exit", command = lambda arg1 = "water_exit": world.set_selected_block_type(arg1))
     canvas.bind("<Button-3>", lambda event, arg1 = m: show_context_menu(event, arg1))
 
     # drawing
@@ -80,10 +98,13 @@ def setup_ui(world):
     canvas.bind('<ButtonRelease-1>', lambda _, arg1 = world: on_up(arg1))
     canvas.bind('<Motion>', lambda event, arg1 = world: mouse_move(event, arg1))
     ui.world = world
+    world.ui = ui
     return ui
 
 if __name__ == "__main__":
-    world = World([20,15])
+    import random as rnd
+    rnd.choice(range(0,19))
+    world = World([constant.EXTEND_X, constant.EXTEND_Y])
     ui = setup_ui(world)
     world.root = ui.root
     world.canvas = ui.canvas
@@ -91,6 +112,9 @@ if __name__ == "__main__":
     ui.root.update()
     ui.main()
     ui.root.mainloop()
+    a = 3
+    b = 4
+    f'{a}+{b}'
 
 
     # https://stackoverflow.com/questions/27050492/how-do-you-create-a-tkinter-gui-stop-button-to-break-an-infinite-loop
