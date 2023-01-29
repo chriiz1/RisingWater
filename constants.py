f12347c1b639c8d0cb8512244c497e6dfee45834
Christoph Proß <christoph.pross@gmx.at>
2023-01-29 20:01:28 +0100

    add sand blocks and much more stuff

--- constants.py
+++ constants.py
@@ -1,16 +1,22 @@
 ## Userinterface, Design
 
+from pickle import FALSE
+
 CANVAS_START=20
 
 WINDOW_WIDTH=1000
 
-WINDOW_HEIGHT=850
+WINDOW_HEIGHT=880
 
 DEFAULT_SPEED = 8
 
 BLOCK_SIZE = 30
 
-BLOCK_COLOR = {"air": "white", "water": "blue", "earth": "brown", "water_entry": "black", "water_exit": "gray"}
+BLOCK_COLOR = {"air": "white", "water": "blue", "earth": "brown", "sand": "yellow",
+                 "water_entry": "black", "water_exit": "gray"}
+
+# BLOCK_TYPE = {"air": AirBlock(), "water": WaterBlock(), "earth": EarthBlock(), "sand": SandBlock(),
+#                 "water_entry": WaterEntryBlock(), "water_exit": WaterExitBlock()}
 
 SPEED_STEPS = 11
 
@@ -25,3 +31,5 @@ SAVE_FOLDER = '/home/christoph/Own_projects/RisingWater/worlds'
 
 AIR_BLOCK_BELOW_CRITICAL_DISTANCE = 2
 AIR_BLOCK_SIDE_CRITICAL_DISTANCE = 4
+
+EROSION = False
